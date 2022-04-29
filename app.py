86881d85adb4aa440e19367530e67f1caf94f2aa
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-04-29 15:53:06 +0300

    add retrieved top headlines from newsapi

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
 
     newsapi = NewsApiClient(api_key='760f167336c24d789133ed6ce44e2dcd')
 
-    
+    top_headlines = newsapi.get_top_headlines(sources = "bbc-news")
 
     return render_template('home.html')
 
