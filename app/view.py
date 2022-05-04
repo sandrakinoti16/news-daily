2575407a69df486f5f76dca8c0f40ba4cec89f12
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-05-04 08:44:01 +0300

    git commit

--- app/view.py
+++ app/view.py
@@ -9,3 +9,7 @@ def home():
     View root page function that returns the index page and its data
     '''
     return render_template('home.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
