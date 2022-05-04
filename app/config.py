00d41b228b4e70ec02d9bcfc51474b93647caa30
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-05-04 23:05:34 +0300

    git commit

--- app/config.py
+++ app/config.py
@@ -3,3 +3,9 @@ class Config:
     General configuration parent class
     '''
     BASE_ARTICLES_URL ='https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG=True
